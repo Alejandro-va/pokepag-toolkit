bfd368588f99ce01cf2ebb6b900b4f0bb0d067ee
Alejandro-va <prof.jahz@gmail.com>
2023-05-22 17:38:57 -0300

    scroll

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## Programación en tiempo real
 
-![texto_alternativo](/src/image/readme.jpg)
+![texto_alternativo](/src/image/back-page.jpg)
 
 npm install react-redux
 npm install @reduxjs/toolkit
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,25 @@
 import React from "react";
+import ScrollButton from "../scrollButton/ScrollButton";
 import { Link } from "react-router-dom";
 import logo from "../../image/pngwing.com.png";
 import "./Header.scss";
 
 const Header = () => {
+  const [scrollHeight, setScrollHeight] = React.useState(0);
+
+  const handleScroll = () => {
+    const position = window.pageYOffset; //obtiene el scroll en y
+    setScrollHeight(position);
+  };
+
+  React.useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+    // eslint-disable-next-line
+  }, []);
   return (
     <div className="header">
       <Link to="/">
@@ -11,6 +27,7 @@ const Header = () => {
           <img src={logo} alt="" />
         </div>
       </Link>
+      <ScrollButton isScrolling={scrollHeight} />
     </div>
   );
 };
